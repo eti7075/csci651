66b59886daed707ebd2366401551b947582d18b7
ethaniannicelli <etiannicelli@gmail.com>
2025-01-27 17:56:15 -0500

    outline requirements, create input.pcap

---
+++ Ethan_Iannicelli_hw1/pktsniffer.py
@@ -0,0 +1,28 @@
+# input.pcap
+
+# --- Output ---
+# Ethernet Header: Packet size, Destination MAC Address, 
+#   Source MAC Address, Ethertype
+# IP Header: Version, Header length, Type of service, Total length, 
+#   Identification, Flags, Fragment offset, Time to live, Protocol, 
+#   Header checksum, Source and Destination IP addresses.
+# Encapsulated Packets: TCP, UDP, or ICMP headers.
+
+# --- Filtering ---
+# support filtering based on the following
+#   host
+#   port
+#   ip
+#   tcp
+#   udp
+#   icmp
+#   net
+
+# use -c argument to limit number of packets analyzed
+
+# example: pktsniffer -r input.pcap -c 5 port 80
+
+# --- Tips ---
+# use argparse to parse command line inputs
+# keep stuff modular (might be reused in the future)
+# use git to show progress
