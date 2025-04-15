0345dd496c47f2ca46c0d1f57f6bc8d6ea7ac1a6
ethaniannicelli <etiannicelli@gmail.com>
2025-04-15 18:15:49 -0400

    trouble shooting intra host conn

--- Ethan_Iannicelli_hw5/layer3_network_code.py
+++ Ethan_Iannicelli_hw5/layer3_network_code.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python
+
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import Node
-from mininet.link import TCLink
-from mininet.log import setLogLevel, info
 from mininet.cli import CLI
+from mininet.link import TCLink
+from mininet.log import setLogLevel
 
 class LinuxRouter(Node):
     def config(self, **params):
@@ -17,48 +19,60 @@ class LinuxRouter(Node):
 class CustomTopo(Topo):
     def build(self):
         # Routers
-        rA = self.addNode('rA', cls=LinuxRouter, ip='20.10.172.129/26')
-        rB = self.addNode('rB', cls=LinuxRouter, ip='20.10.172.1/25')
-        rC = self.addNode('rC', cls=LinuxRouter, ip='20.10.172.193/27')
+        routerA = self.addNode('rA', cls=LinuxRouter, ip='20.10.172.129/26')
+        routerB = self.addNode('rB', cls=LinuxRouter, ip='20.10.172.1/25')
+        routerC = self.addNode('rC', cls=LinuxRouter, ip='20.10.172.193/27')
+
+        # Switches for each LAN
+        sA = self.addSwitch('sA')
+        sB = self.addSwitch('sB')
+        sC = self.addSwitch('sC')
 
         # Hosts for LAN A
         hA1 = self.addHost('hA1', ip='20.10.172.130/26', defaultRoute='via 20.10.172.129')
         hA2 = self.addHost('hA2', ip='20.10.172.131/26', defaultRoute='via 20.10.172.129')
+        self.addLink(hA1, sA)
+        self.addLink(hA2, sA)
+        self.addLink(sA, routerA)
 
         # Hosts for LAN B
         hB1 = self.addHost('hB1', ip='20.10.172.2/25', defaultRoute='via 20.10.172.1')
         hB2 = self.addHost('hB2', ip='20.10.172.3/25', defaultRoute='via 20.10.172.1')
+        self.addLink(hB1, sB)
+        self.addLink(hB2, sB)
+        self.addLink(sB, routerB)
 
         # Hosts for LAN C
         hC1 = self.addHost('hC1', ip='20.10.172.194/27', defaultRoute='via 20.10.172.193')
         hC2 = self.addHost('hC2', ip='20.10.172.195/27', defaultRoute='via 20.10.172.193')
+        self.addLink(hC1, sC)
+        self.addLink(hC2, sC)
+        self.addLink(sC, routerC)
 
-        # LAN links
-        self.addLink(hA1, rA)
-        self.addLink(hA2, rA)
+        # Inter-router links using 20.10.100.0/24
+        self.addLink(routerA, routerB,
+                     intfName1='rA-eth1', intfName2='rB-eth1',
+                     params1={'ip': '20.10.100.1/24'},
+                     params2={'ip': '20.10.100.2/24'})
 
-        self.addLink(hB1, rB)
-        self.addLink(hB2, rB)
+        self.addLink(routerB, routerC,
+                     intfName1='rB-eth2', intfName2='rC-eth1',
+                     params1={'ip': '20.10.100.3/24'},
+                     params2={'ip': '20.10.100.4/24'})
 
-        self.addLink(hC1, rC)
-        self.addLink(hC2, rC)
-
-        # Inter-router network (20.10.100.0/24)
-        self.addLink(rA, rB, intfName1='rA-eth3', intfName2='rB-eth3', params1={'ip': '20.10.100.1/24'}, params2={'ip': '20.10.100.2/24'})
-        self.addLink(rB, rC, intfName1='rB-eth4', intfName2='rC-eth3', params1={'ip': '20.10.100.3/24'}, params2={'ip': '20.10.100.4/24'})
-        self.addLink(rC, rA, intfName1='rC-eth4', intfName2='rA-eth4', params1={'ip': '20.10.100.5/24'}, params2={'ip': '20.10.100.6/24'})
+        self.addLink(routerC, routerA,
+                     intfName1='rC-eth2', intfName2='rA-eth2',
+                     params1={'ip': '20.10.100.5/24'},
+                     params2={'ip': '20.10.100.6/24'})
 
 def run():
     topo = CustomTopo()
     net = Mininet(topo=topo, link=TCLink, controller=None)
     net.start()
 
-    info('\n*** Ping test: hosts in the same LAN should be reachable\n')
-    net.get('hA1').cmdPrint('ping -c 2 20.10.172.131')  # hA1 -> hA2
-    net.get('hB1').cmdPrint('ping -c 2 20.10.172.3')    # hB1 -> hB2
-    net.get('hC1').cmdPrint('ping -c 2 20.10.172.195')  # hC1 -> hC2
+    print("\n=== TESTING LOCAL LAN REACHABILITY ===\n")
+    net.pingAll()
 
-    info('\n*** Launching CLI\n')
     CLI(net)
     net.stop()
 
