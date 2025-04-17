76d14655c13d5aa3499641ca2ad678a088c10a2f
ethaniannicelli <etiannicelli@gmail.com>
2025-04-16 20:16:25 -0400

    add other lans

--- Ethan_Iannicelli_hw5/layer3_network_code.py
+++ Ethan_Iannicelli_hw5/layer3_network_code.py
@@ -4,27 +4,37 @@ from mininet.node import OVSSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel
 
-class LAN_A(Topo):
+class MultiLAN(Topo):
     def build(self):
-        # Create a switch to represent the LAN
-        switch = self.addSwitch('s1')
-        
-        # Host 1 with IP 20.10.172.129/26
-        h1 = self.addHost('h1', ip='20.10.172.129/26')
-        self.addLink(h1, switch)
+        # LAN A
+        sA = self.addSwitch('sA')
+        hA1 = self.addHost('hA1', ip='20.10.172.129/26')
+        hA2 = self.addHost('hA2', ip='20.10.172.130/26')
+        self.addLink(hA1, sA)
+        self.addLink(hA2, sA)
 
-        # Host 2 with IP 20.10.172.130/26
-        h2 = self.addHost('h2', ip='20.10.172.130/26')
-        self.addLink(h2, switch)
+        # LAN B
+        sB = self.addSwitch('sB')
+        hB1 = self.addHost('hB1', ip='20.10.172.1/25')
+        hB2 = self.addHost('hB2', ip='20.10.172.2/25')
+        self.addLink(hB1, sB)
+        self.addLink(hB2, sB)
+
+        # LAN C
+        sC = self.addSwitch('sC')
+        hC1 = self.addHost('hC1', ip='20.10.172.193/27')
+        hC2 = self.addHost('hC2', ip='20.10.172.194/27')
+        self.addLink(hC1, sC)
+        self.addLink(hC2, sC)
 
 def run():
-    topo = LAN_A()
+    topo = MultiLAN()
     net = Mininet(topo=topo, switch=OVSSwitch)
     net.start()
 
-    print("\nTesting connectivity between hosts in LAN A...")
+    print("\n✅ All LANs created. You can test connectivity within LANs.")
+    print("Try: hA1 ping hA2 | hB1 ping hB2 | hC1 ping hC2")
     net.pingAll()
-
     CLI(net)
     net.stop()
 
