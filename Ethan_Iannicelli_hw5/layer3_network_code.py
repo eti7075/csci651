cd5cc6730f03e6cd01a1d96b1b08731a551fe9eb
ethaniannicelli <etiannicelli@gmail.com>
2025-04-16 21:50:54 -0400

    fix typos

--- Ethan_Iannicelli_hw5/layer3_network_code.py
+++ Ethan_Iannicelli_hw5/layer3_network_code.py
@@ -84,17 +84,17 @@ def run():
     hB2.cmd('sudo route add -net 20.10.172.128 netmask 255.255.255.192 gw 20.10.172.1')
     hB2.cmd('sudo route add -net 20.10.172.192 netmask 255.255.255.224 gw 20.10.172.1')
 
-    hB1.cmd('sudo route add -net 20.10.172.0 netmask 255.255.255.128 gw 20.10.172.193')
-    hB1.cmd('sudo route add -net 20.10.172.128 netmask 255.255.255.192 gw 20.10.172.193')
+    hC1.cmd('sudo route add -net 20.10.172.0 netmask 255.255.255.128 gw 20.10.172.193')
+    hC1.cmd('sudo route add -net 20.10.172.128 netmask 255.255.255.192 gw 20.10.172.193')
     hC2.cmd('sudo route add -net 20.10.172.0 netmask 255.255.255.128 gw 20.10.172.193')
     hC2.cmd('sudo route add -net 20.10.172.128 netmask 255.255.255.192 gw 20.10.172.193')
 
     rA.cmd('sudo route add -net 20.10.172.0 netmask 255.255.255.128 gw 20.10.100.2')        
-    rA.cmd('sudo route add -net 20.10.172.192 netmask 255.255.255.128 gw 20.10.100.3')        
-    rB.cmd('sudo route add -net 20.10.172.128 netmask 255.255.255.128 gw 20.10.100.1')        
-    rB.cmd('sudo route add -net 20.10.172.192 netmask 255.255.255.128 gw 20.10.100.3')        
+    rA.cmd('sudo route add -net 20.10.172.192 netmask 255.255.255.224 gw 20.10.100.3')        
+    rB.cmd('sudo route add -net 20.10.172.128 netmask 255.255.255.192 gw 20.10.100.1')        
+    rB.cmd('sudo route add -net 20.10.172.192 netmask 255.255.255.224 gw 20.10.100.3')        
     rC.cmd('sudo route add -net 20.10.172.0 netmask 255.255.255.128 gw 20.10.100.2')        
-    rC.cmd('sudo route add -net 20.10.172.128 netmask 255.255.255.128 gw 20.10.100.1')        
+    rC.cmd('sudo route add -net 20.10.172.128 netmask 255.255.255.192 gw 20.10.100.1')        
 
     CLI(net)
     net.stop()
