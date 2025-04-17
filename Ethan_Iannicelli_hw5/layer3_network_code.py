d244504ea541eb7534225e1bd65146eb88edcd4c
ethaniannicelli <etiannicelli@gmail.com>
2025-04-16 20:49:42 -0400

    reorder things

--- Ethan_Iannicelli_hw5/layer3_network_code.py
+++ Ethan_Iannicelli_hw5/layer3_network_code.py
@@ -1,48 +1,43 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import Node, OVSSwitch
+from mininet.node import Node
+from mininet.log import setLogLevel, info
 from mininet.cli import CLI
-from mininet.log import setLogLevel
+
 
 class LinuxRouter(Node):
     def config(self, **params):
-        super().config(**params)
-        self.cmd('sysctl -w net.ipv4.ip_forward=1')
+        super(LinuxRouter, self).config(**params)
+        self.cmd('sysctl net.ipv4.ip_forward=1')
 
     def terminate(self):
-        self.cmd('sysctl -w net.ipv4.ip_forward=0')
-        super().terminate()
+        self.cmd('sysctl net.ipv4.ip_forward=0')
+        super(LinuxRouter, self).terminate()
 
-class SingleLANWithRouter(Topo):
-    def build(self):
+class NetworkTopo(Topo):
+    def build(self, **_opts):
         switch = self.addSwitch('s1')
 
         # Add router without IP; assign manually after start
-        rA = self.addNode('rA', cls=LinuxRouter)
+        rA = self.addNode('rA', cls=LinuxRouter, ip='20.10.172.129/26')
+    
+        s1 = self.addSwitch('s1')
+    
+        self.addLink(s1, rA, intfName2='rA-eth1', params2={'ip': '20.10.172.129/26'})
 
         # Hosts with IPs in the /26 subnet and rA as gateway
-        hA1 = self.addHost('hA1', ip='20.10.172.130/26', defaultRoute='via 20.10.172.129')
-        hA2 = self.addHost('hA2', ip='20.10.172.131/26', defaultRoute='via 20.10.172.129')
+        hA1 = self.addHost(name='hA1', ip='20.10.172.130/26', defaultRoute='via 20.10.172.129')
+        hA2 = self.addHost(name='hA2', ip='20.10.172.131/26', defaultRoute='via 20.10.172.129')
 
         # Connect everything to the switch
         self.addLink(hA1, switch)
         self.addLink(hA2, switch)
-        self.addLink(rA, switch)
 
 def run():
-    topo = SingleLANWithRouter()
-    net = Mininet(topo=topo, switch=OVSSwitch, controller=None)
+    topo = NetworkTopo()
+    net = Mininet(topo=topo)
+
     net.start()
-
-    rA = net.get('rA')
-    # Assign IP manually to router's interface
-    rA.setIP('20.10.172.129/26', intf=rA.intfNames()[0])
-
-    print("\n✅ LAN A is up. Try:")
-    print("  hA1 ping hA2")
-    print("  hA1 ping rA")
-    print("  hA2 ping rA")
-
     CLI(net)
     net.stop()
 
