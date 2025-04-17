888b3713065dccbf4e8f49b0a5951389ac56c37d
ethaniannicelli <etiannicelli@gmail.com>
2025-04-16 20:21:16 -0400

    add routers

--- Ethan_Iannicelli_hw5/layer3_network_code.py
+++ Ethan_Iannicelli_hw5/layer3_network_code.py
@@ -1,40 +1,58 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import OVSSwitch
+from mininet.node import Node, OVSSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel
 
-class MultiLAN(Topo):
+class LinuxRouter(Node):
+    # Enable forwarding on router
+    def config(self, **params):
+        super().config(**params)
+        self.cmd('sysctl -w net.ipv4.ip_forward=1')
+
+    def terminate(self):
+        self.cmd('sysctl -w net.ipv4.ip_forward=0')
+        super().terminate()
+
+class MultiLANWithRouters(Topo):
     def build(self):
-        # LAN A
-        sA = self.addSwitch('s1')
-        hA1 = self.addHost('hA1', ip='20.10.172.129/26')
-        hA2 = self.addHost('hA2', ip='20.10.172.130/26')
+        # ---------------- LAN A ----------------
+        sA = self.addSwitch('sA')
+        rA = self.addNode('rA', cls=LinuxRouter, ip='20.10.172.129/26')  # router in LAN A
+        hA1 = self.addHost('hA1', ip='20.10.172.130/26', defaultRoute='via 20.10.172.129')
+        hA2 = self.addHost('hA2', ip='20.10.172.131/26', defaultRoute='via 20.10.172.129')
+
         self.addLink(hA1, sA)
         self.addLink(hA2, sA)
+        self.addLink(rA, sA)
 
-        # LAN B
-        sB = self.addSwitch('s2')
-        hB1 = self.addHost('hB1', ip='20.10.172.1/25')
-        hB2 = self.addHost('hB2', ip='20.10.172.2/25')
+        # ---------------- LAN B ----------------
+        sB = self.addSwitch('sB')
+        rB = self.addNode('rB', cls=LinuxRouter, ip='20.10.172.1/25')
+        hB1 = self.addHost('hB1', ip='20.10.172.2/25', defaultRoute='via 20.10.172.1')
+        hB2 = self.addHost('hB2', ip='20.10.172.3/25', defaultRoute='via 20.10.172.1')
+
         self.addLink(hB1, sB)
         self.addLink(hB2, sB)
+        self.addLink(rB, sB)
 
-        # LAN C
-        sC = self.addSwitch('s3')
-        hC1 = self.addHost('hC1', ip='20.10.172.193/27')
-        hC2 = self.addHost('hC2', ip='20.10.172.194/27')
+        # ---------------- LAN C ----------------
+        sC = self.addSwitch('sC')
+        rC = self.addNode('rC', cls=LinuxRouter, ip='20.10.172.193/27')
+        hC1 = self.addHost('hC1', ip='20.10.172.194/27', defaultRoute='via 20.10.172.193')
+        hC2 = self.addHost('hC2', ip='20.10.172.195/27', defaultRoute='via 20.10.172.193')
+
         self.addLink(hC1, sC)
         self.addLink(hC2, sC)
+        self.addLink(rC, sC)
 
 def run():
-    topo = MultiLAN()
-    net = Mininet(topo=topo, switch=OVSSwitch)
+    topo = MultiLANWithRouters()
+    net = Mininet(topo=topo, switch=OVSSwitch, controller=None)
     net.start()
 
-    print("\n✅ All LANs created. You can test connectivity within LANs.")
-    print("Try: hA1 ping hA2 | hB1 ping hB2 | hC1 ping hC2")
-    net.pingAll()
+    print("\nRouters connected to each LAN.")
+    print("Try pinging between hosts in each LAN, like: hA1 ping hA2")
     CLI(net)
     net.stop()
 
